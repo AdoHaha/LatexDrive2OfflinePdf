beec41f38a9e3848b1bf113b606fa8c4342af35d
Igor <igorzubrycki@gmail.com>
2014-12-30 23:22:14 +0100

    clear some utf-8 edge cases

--- compile_from_drive.sh
+++ compile_from_drive.sh
@@ -1,5 +1,5 @@
 
-phantomjs plain_text.js $1 | tail -n +2 | head -n -1 >"$3"/"$2".tex
+phantomjs plain_text.js $1 | tail -n +2 | head -n -1|iconv -f utf-8 -t utf-8 -c >"$3"/"$2".tex
 
 
 cd $3
@@ -8,8 +8,8 @@ cd $3
 
 
 
-if [-n "$4"]; then
-        $compiler_name=$4
+if [ -n $4 ]; then
+        compiler_name="$4"
 else
         compiler_name="pdflatex"
 fi
--- plain_text.js
+++ plain_text.js
@@ -4,9 +4,9 @@ var page = webPage.create();
 var system = require('system');
 
 
+var settings={encoding: "utf8"};
 
-
-page.open(system.args[1], function (status) {
+page.open(system.args[1], settings,function (status) {
   console.log(page.plainText);
   phantom.exit();
 });
